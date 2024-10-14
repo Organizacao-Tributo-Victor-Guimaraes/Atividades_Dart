177057784019af5ea46adf10d10ba4c71b99176b
Raffael Guideti Miello <raffaelguideti@gmail.com>
2024-10-13 23:25:59 -0300

    Adicionando Funções
    Adicionando função de colocar números inteiros, ainda só funciona com números específicos

--- atividade_10/main.dart
+++ atividade_10/main.dart
@@ -1,12 +1,72 @@
 import 'dart:io';
 
+
+Map<int, String> inteiroRomano = {
+  1000 : 'M',
+  900 : 'CM',
+  500 : 'D',
+  400 : 'CD',
+  100 : 'C',
+  90 : 'XC',
+  50 : 'L',
+  40 : 'XL',
+  10 : 'X',
+  9 : 'IX',
+  5 : 'V',
+  4 : 'IV',
+  1 : 'I'
+}; 
+
+Map<String, int> romanoInteiro = {
+  'M': 1000,
+  'CM': 900,
+  'D': 500,
+  'CD': 400,
+  'C': 100,
+  'XC': 90,
+  'L': 50,
+  'XL': 40,
+  'X': 10,
+  'IX': 9,
+  'V': 5,
+  'IV': 4,
+  'I': 1,
+};
+
+void converteRomano (int inteiro){
+  if (inteiroRomano.containsKey(inteiro)) {
+    print('O número $inteiro é representado por ${inteiroRomano[inteiro]}');
+  } else {
+    print('Valor $inteiro não encontrado.');
+  }
+}
+
+void converteInteiro(String romano) {
+  int total = 0;
+  int i = 0;
+
+  while (i < romano.length) {
+   
+    if (i + 1 < romano.length && romanoInteiro.containsKey(romano.substring(i, i + 2))) {
+      total += romanoInteiro[romano.substring(i, i + 2)]!;
+      i += 2; 
+    } else {
+      total += romanoInteiro[romano[i]]!;
+      i++; 
+    }
+  }
+
+  print('O número romano $romano é igual a $total');
+}
+
 void main(){
   
-  int valor;
+  int valori;
+  String valorr;
   int escolha;
 
 
-  print('Seja bem vindo a calculadora de numeros romanos \n');
+  print('Seja bem vindo ao conversor de numeros romanos \n');
   print('O que deseja converter? \n');
   print('Digite 1 para converter inteiros para romanos');
   print('Digite 2 para converter romanos para inteiros');
@@ -21,10 +81,12 @@ void main(){
   switch(escolha){
     case 1 :
     print('Digite o valor que deseja converter para romano');
-    valor = int.parse(stdin.readLineSync()!);
+    valori = int.parse(stdin.readLineSync()!);
+    converteRomano(valori);
 
     case 2 :
     print('Digite o valor que deseja converter para inteiro');
-    valor = int.parse(stdin.readLineSync()!);
+    valorr = stdin.readLineSync()!;
+    converteInteiro(valorr);
   }
 }
